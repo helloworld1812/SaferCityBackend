fa5c6b080395b50f82b4db949e01442fb389d9dc
Yan B <yan@gethello.world>
2017-04-24 21:25:48 -0700

    Format code - fix lint errors

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-
   if (req.url === '/api/events') {
     res.writeHead(200, { 'Content-Type': 'application/json' });
     const event = {
-      title: 'Free wine!'
+      title: 'Free wine!',
     };
     res.end(JSON.stringify(event));
-  }
-  else {
+  } else {
     res.writeHead(404);
     res.end();
   }
-
 }).listen(8080, '127.0.0.1');
--- app.test.js
+++ app.test.js
@@ -3,20 +3,20 @@ require('./app.js');
 
 const APP_URL = 'http://127.0.0.1:8080';
 
-http.get(APP_URL + '/api/events', res => {
+http.get(`${APP_URL}/api/events`, (res) => {
   res.on('data', (event) => {
     if (JSON.parse(event).title === 'Free wine!') {
-      console.log("PASS");
+      console.log('PASS');
     } else {
-      console.log("FAIL");
+      console.log('FAIL');
     }
   });
 });
 
-http.get(APP_URL, res => {
-  if (res.statusCode == 404) {
-    console.log("PASS");
+http.get(APP_URL, (res) => {
+  if (res.statusCode === 404) {
+    console.log('PASS');
   } else {
-    console.log("FAIL");
+    console.log('FAIL');
   }
 });
