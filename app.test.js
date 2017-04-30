09bb1a95e659a6feb759eb6223470804742ae73d
Yan B <yan@gethello.world>
2017-04-29 21:29:47 -0700

    Connect “evens/routes.js” to “events/service.js”

--- app.test.js
+++ app.test.js
@@ -1,7 +1,7 @@
 /* global test, expect, afterAll */
 // Load and run the app
 const runningApp = require('./app');
-const request = require('request');
+const request = require('superagent');
 
 const APP_URL = 'http://127.0.0.1:8080';
 
@@ -9,37 +9,97 @@ afterAll(() => {
   runningApp.close();
 });
 
-test('/events get returns 500', (done) => {
-  request.get(`${APP_URL}/events`).on('response', (res) => {
-    expect(res.statusCode).toBe(500);
-    done();
-  });
+// Please note: test order matters
+
+const event1 = { title: 'Event 1' };
+const event2 = { title: 'Event 2' };
+const id1 = 1;
+const id2 = 2;
+
+test('/events POST returns id of newly created item', () => (
+  // code below returns promise
+  request.post(`${APP_URL}/events`)
+    .set('Content-Type', 'application/json')
+    .send(event1)
+    .then((resp) => {
+      expect(resp.body).toEqual(id1);
+    })
+));
+
+test('/events POST returns id of newly created item (one more time)', () => (
+  // code below returns promise
+  request.post(`${APP_URL}/events`)
+    .set('Content-Type', 'application/json')
+    .send(event2)
+    .then((resp) => {
+      expect(resp.body).toEqual(id2);
+    })
+));
+
+test('/events GET returns 2 items', () => (
+  // code below returns promise
+  request.get(`${APP_URL}/events`)
+    .then((resp) => {
+      const events = resp.body;
+      expect(events.length).toEqual(2);
+      expect(events[0]).toEqual(event1);
+      expect(events[1]).toEqual(event2);
+    })
+));
+
+test('/events/:id GET returns event for existing id', () => (
+  // code below returns promise
+  request.get(`${APP_URL}/events/${id1}`)
+    .then((res) => {
+      const event = res.body;
+      expect(event).toEqual(event1);
+    })
+));
+
+test('/events/:id GET returns 404 error for non-existing id', () => (
+  // code below returns promise
+  request.get(`${APP_URL}/events/-1`)
+    .catch((res) => {
+      expect(res.status).toBe(404);
+    })
+));
+
+test('/events/:id PUT updates existing id', () => {
+  event1.title = 'Updated event';
+  return request.put(`${APP_URL}/events/${id1}`)
+    .set('Content-Type', 'application/json')
+    .send(event1)
+    .then(() => (
+      // When we've updated we would like to check that event is indeed updated
+      // So we issue a request to fetch just modified data...
+      request.get(`${APP_URL}/events/${id1}`)
+    ))
+    .then((res) => {
+      // ... and then check that it's indeed equal to an updated event
+      const event = res.body;
+      expect(event).toEqual(event1);
+    });
 });
 
-test('/events/1 get returns 500', (done) => {
-  request.get(`${APP_URL}/events/1`).on('response', (res) => {
-    expect(res.statusCode).toBe(500);
-    done();
-  });
-});
+test('/events/:id PUT returns 404 error for non-existing id', () => (
+  // code below returns promise
+  request.put(`${APP_URL}/events/-1`)
+    .set('Content-Type', 'application/json')
+    .send(event1)
+    .catch((res) => {
+      expect(res.status).toBe(404);
+    })
+));
 
-test('/events post returns 500', (done) => {
-  request.post(`${APP_URL}/events`, { form: { title: 'New event' } }).on('response', (res) => {
-    expect(res.statusCode).toBe(500);
-    done();
-  });
-});
-
-test('/events/1 put returns 500', (done) => {
-  request.put(`${APP_URL}/events/1`).on('response', (res) => {
-    expect(res.statusCode).toBe(500);
-    done();
-  });
-});
-
-test('/events/1 delete returns 500', (done) => {
-  request.delete(`${APP_URL}/events/1`).on('response', (res) => {
-    expect(res.statusCode).toBe(500);
-    done();
-  });
-});
+test('/events/1 delete returns 500', () => (
+  request.delete(`${APP_URL}/events/${id1}`)
+    .then(() => (
+      // When we've removed the event we would like to check that it's indeed removed
+      // So we issue a request to fetch list of events...
+      request.get(`${APP_URL}/events`)
+    ))
+    .then((resp) => {
+      const events = resp.body;
+      expect(events.length).toEqual(1);
+    })
+));
--- events/routes.js
+++ events/routes.js
@@ -1,28 +1,51 @@
 const express = require('express');
 const service = require('./service');
+const bodyParser = require('body-parser');
 
 const router = express.Router();
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
 
 router.post('/', (req, res) => {
-  res.status(500).send('Not implemented');
+  const event = req.body;
+  const id = service.create(event);
+  res.json(id);
 });
 
 router.get('/', (req, res) => {
-  res.status(500).send('Not implemented');
+  const events = service.list();
+  res.json(events);
 });
 
 router.get('/:id', (req, res) => {
-  res.status(500).send('Not implemented');
+  const id = req.params.id;
+  try {
+    const event = service.find(id);
+    res.json(event);
+  } catch (e) {
+    res.status(404).send(`No event with id "${id}" exists`);
+  }
 });
 
 router.put('/:id', (req, res) => {
-  res.status(500).send('Not implemented');
+  const id = req.params.id;
+  try {
+    const event = req.body;
+    service.update(id, event);
+    res.send();
+  } catch (e) {
+    res.status(404).send(`No event with id "${id}" exists`);
+  }
 });
 
 router.delete('/:id', (req, res) => {
-  res.status(500).send('Not implemented');
+  const id = req.params.id;
+  try {
+    service.remove(id);
+    res.send();
+  } catch (e) {
+    res.status(404).send(`No event with id "${id}" exists`);
+  }
 });
-
-router.get('/', () => service.list());
 
 module.exports = router;
