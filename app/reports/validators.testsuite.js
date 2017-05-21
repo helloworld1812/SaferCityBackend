565a03ed709f4589991257ae47a7ff36c87864e7
Yan B <yan@gethello.world>
2017-05-21 00:45:56 -0700

    Make report validation tests a bit more readable by using util functions

--- app/reports/validators.testsuite.js
+++ app/reports/validators.testsuite.js
@@ -8,6 +8,11 @@ const request = require('superagent');
 
 const APP_URL = global.appUrl;
 
+/** Utility function that returns errors for specified field. */
+const getErrorsForField = (errors, field) => errors.filter(e => e.param === field);
+/** Utility function that returns errors which messages contain specified text. */
+const getErrorsContainingText = (errors, text) => errors.filter(e => e.msg.includes(text));
+
 test('/reports POST returns error for report with no title', () => (
   // code below returns promise
   request.post(`${APP_URL}/reports`)
@@ -22,10 +27,10 @@ test('/reports POST returns error for report with no title', () => (
 
       const errors = JSON.parse(resp.response.text);
 
-      const titleErrors = errors.filter(e => e.param === 'title');
+      const titleErrors = getErrorsForField(errors, 'title');
       expect(titleErrors.length).toBeGreaterThan(0); // At least 1 error message
 
-      const titleErrorsAboutRequired = titleErrors.filter(e => e.msg.includes('required'));
+      const titleErrorsAboutRequired = getErrorsContainingText(titleErrors, 'required');
       expect(titleErrorsAboutRequired.length).toBe(1);
     })
 ));
@@ -44,10 +49,10 @@ test('/reports POST returns error for report with no location', () => (
 
       const errors = JSON.parse(resp.response.text);
 
-      const locationErrors = errors.filter(e => e.param === 'location');
+      const locationErrors = getErrorsForField(errors, 'location');
       expect(locationErrors.length).toBeGreaterThan(0); // At least 1 error message
 
-      const locationErrorsAboutRequired = locationErrors.filter(e => e.msg.includes('required'));
+      const locationErrorsAboutRequired = getErrorsContainingText(locationErrors, 'required');
       expect(locationErrorsAboutRequired.length).toBe(1);
     })
 ));
@@ -66,10 +71,10 @@ test('/reports POST returns error for report with no time', () => (
 
       const errors = JSON.parse(resp.response.text);
 
-      const timeErrors = errors.filter(e => e.param === 'time');
+      const timeErrors = getErrorsForField(errors, 'time');
       expect(timeErrors.length).toBeGreaterThan(0); // At least 1 error message
 
-      const timeErrorsAboutRequired = timeErrors.filter(e => e.msg.includes('required'));
+      const timeErrorsAboutRequired = getErrorsContainingText(timeErrors, 'required');
       expect(timeErrorsAboutRequired.length).toBe(1);
     })
 ));
@@ -89,10 +94,10 @@ test('/reports POST returns error for report with not valid time', () => (
 
       const errors = JSON.parse(resp.response.text);
 
-      const timeErrors = errors.filter(e => e.param === 'time');
+      const timeErrors = getErrorsForField(errors, 'time');
       expect(timeErrors.length).toBeGreaterThan(0); // At least 1 error message
 
-      const timeErrorsAboutValid = timeErrors.filter(e => e.msg.includes('valid'));
+      const timeErrorsAboutValid = getErrorsContainingText(timeErrors, 'valid');
       expect(timeErrorsAboutValid.length).toBe(1);
     })
 ));
