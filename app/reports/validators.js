3e5a4beca9c4a0b38239f2bb9d1b02e059cc94bd
Yan B <yan@gethello.world>
2017-05-21 02:07:02 -0700

    Add length validation for report title, location and details.

--- app/reports/validators.js
+++ app/reports/validators.js
@@ -2,9 +2,12 @@ const REQUIRED_VALIDATION_ERROR_MESSAGE = 'Field is required but no value is pro
 
 const reportValidator = (req, res, next) => {
   req.checkBody('title', REQUIRED_VALIDATION_ERROR_MESSAGE).notEmpty();
+  req.checkBody('title', 'Must be less than 80 chars long').len(0, 80);
   req.checkBody('location', REQUIRED_VALIDATION_ERROR_MESSAGE).notEmpty();
+  req.checkBody('location', 'Must be less than 80 chars long').len(0, 80);
   req.checkBody('time', REQUIRED_VALIDATION_ERROR_MESSAGE).notEmpty();
   req.checkBody('time', 'Provided time is not a valid time').isDate();
+  req.checkBody('details', 'Must be less than 255 chars long').len(0, 255);
 
   req.getValidationResult().then((result) => {
     if (result.isEmpty()) {
--- app/reports/validators.testsuite.js
+++ app/reports/validators.testsuite.js
@@ -35,6 +35,30 @@ test('/reports POST returns error for report with no title', () => (
     })
 ));
 
+test('/reports POST returns error for report with too long title', () => (
+  // code below returns promise
+  request.post(`${APP_URL}/reports`)
+    .set('Content-Type', 'application/json')
+    .send({
+      title: 'Most of the asteroids are not spherical because their gravity is not strong enough '
+        + 'to form a sphere from rock.',
+      location: 'Santana Row',
+      time: new Date(2017, 5, 11, 20, 0, 0, 0),
+      details: 'Bar surfing',
+    })
+    .catch((resp) => {
+      expect(resp.status).toBe(422);
+
+      const errors = JSON.parse(resp.response.text);
+
+      const fieldErrors = getErrorsForField(errors, 'title');
+      expect(fieldErrors.length).toBeGreaterThan(0); // At least 1 error message
+
+      const fieldErrorsWithText = getErrorsContainingText(fieldErrors, 'long');
+      expect(fieldErrorsWithText).toHaveLength(1);
+    })
+));
+
 test('/reports POST returns error for report with no location', () => (
   // code below returns promise
   request.post(`${APP_URL}/reports`)
@@ -53,6 +77,30 @@ test('/reports POST returns error for report with no location', () => (
       expect(fieldErrors.length).toBeGreaterThan(0); // At least 1 error message
 
       const fieldErrorsWithText = getErrorsContainingText(fieldErrors, 'required');
+      expect(fieldErrorsWithText).toHaveLength(1);
+    })
+));
+
+test('/reports POST returns error for report with too long location', () => (
+  // code below returns promise
+  request.post(`${APP_URL}/reports`)
+    .set('Content-Type', 'application/json')
+    .send({
+      title: 'Why asteroids are not spherical',
+      location: 'Asteroids are located in "asteroid belt" between Mars and Jupiter where strong'
+      + 'jovial gravity prevent them from forming a full-fledged planet.',
+      time: new Date(2017, 5, 11, 20, 0, 0, 0),
+      details: 'Bar surfing',
+    })
+    .catch((resp) => {
+      expect(resp.status).toBe(422);
+
+      const errors = JSON.parse(resp.response.text);
+
+      const fieldErrors = getErrorsForField(errors, 'location');
+      expect(fieldErrors.length).toBeGreaterThan(0); // At least 1 error message
+
+      const fieldErrorsWithText = getErrorsContainingText(fieldErrors, 'long');
       expect(fieldErrorsWithText).toHaveLength(1);
     })
 ));
@@ -98,6 +146,32 @@ test('/reports POST returns error for report with not valid time', () => (
       expect(fieldErrors.length).toBeGreaterThan(0); // At least 1 error message
 
       const fieldErrorsWithText = getErrorsContainingText(fieldErrors, 'valid');
+      expect(fieldErrorsWithText).toHaveLength(1);
+    })
+));
+
+test('/reports POST returns error for report with too long description', () => (
+  // code below returns promise
+  request.post(`${APP_URL}/reports`)
+    .set('Content-Type', 'application/json')
+    .send({
+      title: 'Why asteroids are not spherical',
+      location: 'Asteroid belt',
+      time: new Date(2017, 5, 11, 20, 0, 0, 0),
+      details: 'There is asteroid that is spherical - Ceres. Its radius is 945 km and '
+        + 'it has strong enough gravity to round the dwarf planet. How does gravity '
+        + 'related to roundness? Big asteroids are forming by gathering smaller '
+        + 'asteroids (accretion). So asteroid is a big pile of rocks.',
+    })
+    .catch((resp) => {
+      expect(resp.status).toBe(422);
+
+      const errors = JSON.parse(resp.response.text);
+
+      const fieldErrors = getErrorsForField(errors, 'details');
+      expect(fieldErrors.length).toBeGreaterThan(0); // At least 1 error message
+
+      const fieldErrorsWithText = getErrorsContainingText(fieldErrors, 'long');
       expect(fieldErrorsWithText).toHaveLength(1);
     })
 ));
